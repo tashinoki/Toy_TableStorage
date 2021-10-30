c024dffbf614eeb55b11fdebc454542dff132f0b
tashinoki <kenya.112163@gmail.com>
2021-10-30 13:06:42 +0900

    insert entity

--- InsertEntity.cs
+++ InsertEntity.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
@@ -16,8 +17,25 @@ namespace Toy_CloudStorage
         [FunctionName("InsertEntity")]
         public static async Task Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
-            [Table("ThroughputTest", Connection = "TableStorage")] CloudTable configTable,
+            [Table("ThroughputTest", Connection = "TableStorage")] CloudTable client,
             ILogger log)
+        {
+            var prtitionKey = Guid.NewGuid().ToString("N");
+            
+            foreach(var i in Enumerable.Range(1, 10001))
+            {
+                var entity = new Entity
+                {
+                    PartitionKey = prtitionKey,
+                    RowKey = i.ToString("N")
+                };
+                await client.ExecuteAsync(
+                    TableOperation.Insert(entity)
+                );
+            }
+        }
+
+        private class Entity: TableEntity
         {
         }
     }
